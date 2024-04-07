47ba35888d59124174fdbcb48be8ecec4e99a2d2
dancolm95 <dancolm95@gmail.com>
2024-04-07 00:04:50 -0500

    FIXED FILE

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""Module to interact with an api"""
+""" Module that display data of the employee """
 import requests
 import sys
 
-url = "https://jsonplaceholder.typicode.com"
+API = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == "__main__":
-    response_todos = requests.get(f"{url}/todos?userId={sys.argv[1]}")
-    response_users = requests.get(f"{url}/users/{sys.argv[1]}")
+    # Obtener informacion de un empleado en particular
+    RES_TASK = requests.get(f'{API}/todos?userId={sys.argv[1]}')
+    RES_USERS = requests.get(f'{API}/users/{sys.argv[1]}')
 
-    todos = response_todos.json()
-    user = response_users.json().get('name')
+    # deserializado de datos
+    tasks = RES_TASK.json()
+    users = RES_USERS.json()
 
-    done_taks = list()
-    for todo in todos:
-        if todo.get('completed'):
-            done_taks.append(todo)
-    print(
-        f"Employee {user} is done with tasks({len(done_taks)}/{len(todos)}):")
-    for todo in done_taks:
-        print(f"\t {todo.get('title')}")
+    completed = []
+    # iteramos las task completadas y lo guardamos en completed.
+    for task in tasks:
+        if task.get('completed'):
+            completed.append(task)
+
+    # Empleado y cantidad de tareas completadas y total
+    print(f"Employee {users.get('name')} is done with\
+ tasks({len(completed)}/{len(tasks)}):")
+
+    # Iteramos todas las tareas completadas.
+    for t in completed:
+        print(f"\t {t.get('title')}")
